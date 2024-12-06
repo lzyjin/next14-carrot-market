6a34f4c23f5c7e9bf4987b5b9ed4a88c00c9f0ee
lzyjin <1247y.j@gmail.com>
2024-12-06 20:08:55 +0900

    eslint, typescript ignore error 코드 주석

--- next.config.mjs
+++ next.config.mjs
@@ -11,14 +11,14 @@ const nextConfig = {
     ],
   },
   eslint: {
-    ignoreDuringBuilds: true,
+    // ignoreDuringBuilds: true,
   },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
     // !! WARN !!
-    ignoreBuildErrors: true,
+    // ignoreBuildErrors: true,
   },
 };
 
